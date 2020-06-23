6e1e21e1b10dfdcdef3d0db7f34563198ce6a1d2
Conor Barnes <conorbarnes93@gmail.com>
2020-06-23 15:35:57 -0700

    Create todolist module pattern

--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ import './styles.css';
 const todoitem = (title, description, dueDate, priority) => {
     const status = "incomplete"
     let tags = []
-    console.log(this)
     let addTag = (tag) => {
-        console.log(this)
         tags.push(tag)
     }
     let deleteTag = (tag) => {
         for (let i = 0; i < tags.length; i++) {
             if (tags[i] === tag) {
                 tags.splice(i, 1);
+                break;
             }
         }
     }
@@ -27,4 +26,28 @@ fakeitem.addTag('Pets')
 console.log(fakeitem.tags)
 fakeitem.addTag('Exercise')
 console.log(fakeitem.tags)
-fakeitem.addTag()
+fakeitem.addTag('Boring')
+console.log(fakeitem.tags)
+fakeitem.deleteTag('Exercise')
+console.log(fakeitem.tags)
+
+const todolist = (() => {
+    const list = []
+    let projectSort = tag => {
+        let sortedList = []
+        return sortedList;
+    }
+    let addToList = item => {
+        list.push(item)
+    }
+    let deleteFromList = item => {
+        for (let i = 0; i < list.length; i++) {
+            if (list[i] === list) {
+                list.splice(i, 1);
+                break;
+            }
+        }
+    }
+
+    return {list, addToList, deleteFromList, projectSort}
+})();
